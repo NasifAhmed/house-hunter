3511eb18878287d296788c2cf039d5f54840cc7c
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-01-24 11:15:03 +0600

    Add house card components

--- client/src/components/HouseCard.tsx
+++ client/src/components/HouseCard.tsx
@@ -6,19 +6,82 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { houseType } from "@/types/types";
+import { Button } from "./ui/button";
 
-export default function HouseCard() {
+export default function HouseCard({ data }: { data: houseType }) {
     return (
-        <Card>
+        <Card className="z-20 max-w-lg w-full">
             <CardHeader>
-                <CardTitle>House</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+                <CardTitle className="text-2xl">{data.name}</CardTitle>
+                <CardDescription></CardDescription>
             </CardHeader>
             <CardContent>
-                <p>Card Content</p>
+                <div className="text-lg mb-4">
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Availability Date :{" "}
+                        </span>
+                        {data.availibility_date}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Address :{" "}
+                        </span>
+                        {data.address}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            City :{" "}
+                        </span>
+                        {data.city}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Bedrooms :{" "}
+                        </span>
+                        {data.bedrooms}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Bathrooms :{" "}
+                        </span>
+                        {data.bathrooms}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Room Size :{" "}
+                        </span>
+                        {data.room_size}
+                        {" sqft"}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Rent per month :{" "}
+                        </span>
+                        {data.rent}
+                    </h3>
+                    <h3>
+                        <span className="font-bold text-foreground">
+                            Phone number :{" "}
+                        </span>
+                        {data.phone_number}
+                    </h3>
+                </div>
+                <div className=" max-h-80 mb-6 overflow-hidden">
+                    <img
+                        className="w-full object-cover object-center"
+                        src={data.picture}
+                        alt=""
+                    />
+                </div>
+                <h3>{data.desc}</h3>
             </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardFooter className="flex justify-between">
+                <Button>Details</Button>
+                {/* {userFromDB?.role === "participant" && (
+                    <Button>Join Camp</Button>
+                )} */}
             </CardFooter>
         </Card>
     );
