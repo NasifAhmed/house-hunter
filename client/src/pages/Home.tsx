f20103c9c53539aa5d9d74ad340652d974c189f1
Nasif Ahmed <nasif2ahmed@gmail.com>
2024-01-24 11:15:29 +0600

    Setup house cards to show on homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,3 +1,30 @@
+import HouseCard from "@/components/HouseCard";
+import { useAxios } from "@/hooks/useAxios";
+import { houseType } from "@/types/types";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return <div>Home</div>;
+    const axios = useAxios();
+    const [houseData, setHouseData] = useState<houseType[]>();
+    useEffect(() => {
+        getHouseData();
+    }, []);
+
+    async function getHouseData() {
+        await axios.get("/house").then((response) => {
+            console.log("Fetched house data");
+            setHouseData(response.data);
+            console.log(houseData);
+        });
+    }
+    return (
+        <>
+            <div className="w-full grid grid-cols-2 gap-2">
+                {houseData &&
+                    houseData.map((house, index) => {
+                        return <HouseCard data={house} key={index} />;
+                    })}
+            </div>
+        </>
+    );
 }
